6fa7ab2a1e6f8108ce2956b070207c59e96328eb
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-01-02 18:19:59 -0300

    docs: mejora con do while

--- AdivinarNumero/Program.cs
+++ AdivinarNumero/Program.cs
@@ -8,24 +8,25 @@ namespace AdivinarNumero
         {
             Random rand = new Random();
             int randNumber =rand.Next(0,101);
+            int number;
             Console.WriteLine("Bienvenido a adivina el número");
-            int number = 102;
-            while (number != randNumber)
-            {
-                Console.WriteLine("Escribe un número del 0 al 100");
+            Console.WriteLine("Escribe un número del 0 al 100");
+            do {
+                number = Int32.Parse(Console.ReadLine());
                 // verifica si el usuario ingresó un número
-                while (!Int32.TryParse(Console.ReadLine(), out number))
-                {
-                    Console.WriteLine("Operación inválida. Ingrese entre 0 a 100");
-                };
+                //while (!Int32.TryParse(Console.ReadLine(), out number))
+                //{
+                //    Console.WriteLine("Operación inválida. Ingrese entre 0 a 100");
+                //};
+
                 // verifica que el usuario ingrese entre 0 al 100
-                if (number >= 0 &&  number <= 100) 
+                if (number >= 0 && number <= 100)
                 {
                     if (number < randNumber) Console.WriteLine($"El número {number} es menor al número correcto");
                     else Console.WriteLine($"El número {number} es mayor al número correcto");
                 }
                 else Console.WriteLine("Número inválido. Debe elegir entre 0 a 100");
-            }
+            } while (number != randNumber);
             Console.WriteLine("FELICIDADES ACERTASTE EL NÚMERO");
         }
     }
