43c220946ba38b314837ba7e8955ac390008b9ee
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-01-02 18:28:55 -0300

    docs: try catch

--- AdivinarNumero/Program.cs
+++ AdivinarNumero/Program.cs
@@ -12,7 +12,22 @@ namespace AdivinarNumero
             Console.WriteLine("Bienvenido a adivina el número");
             Console.WriteLine("Escribe un número del 0 al 100");
             do {
+                try
+                {
                 number = Int32.Parse(Console.ReadLine());
+                if (number >= 0 && number <= 100)
+                {
+                    if (number < randNumber) Console.WriteLine($"El número {number} es menor al número correcto");
+                    else Console.WriteLine($"El número {number} es mayor al número correcto");
+                }
+                else Console.WriteLine("Número inválido. Debe elegir entre 0 a 100");
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ingreso inválido. Ingrese entre 0 a 100");
+                    number = 0;
+                }
                 // verifica si el usuario ingresó un número
                 //while (!Int32.TryParse(Console.ReadLine(), out number))
                 //{
@@ -20,12 +35,6 @@ namespace AdivinarNumero
                 //};
 
                 // verifica que el usuario ingrese entre 0 al 100
-                if (number >= 0 && number <= 100)
-                {
-                    if (number < randNumber) Console.WriteLine($"El número {number} es menor al número correcto");
-                    else Console.WriteLine($"El número {number} es mayor al número correcto");
-                }
-                else Console.WriteLine("Número inválido. Debe elegir entre 0 a 100");
             } while (number != randNumber);
             Console.WriteLine("FELICIDADES ACERTASTE EL NÚMERO");
         }
